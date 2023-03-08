f04a3a28942471ec097f617fa3c8637570ac57f9
maxime-42 <mkayumba@student.42.fr>
2023-03-08 17:32:39 +0100

    ex01 day00 is oki

--- day00/ex01/exec.py
+++ day00/ex01/exec.py
@@ -1,7 +1,23 @@
 #!/usr/bin/python3
-"""take argument aand print it"""
+"""take input argument 
+	reove the progrmq name reverse
+ 	If no argument are provided, do nothing
+	If more than one argument are provided, merge them 
+    into a single string with each argument separated by a space character.
+    by reverse oreder of argument and alphabe upcase to lowercase vice versa
+    exemple : 
+		input     == ['Hello', 'my Friend']
+		first rev  == ['my Friend', 'Hello']
+		second rev == 'hELLO MY fRIEND'
+		third      == 'DNEIRf YM OLLEh'
+"""
 
 import sys
 
-TOTAL = len(sys.argv)
-print(f"number augument = {TOTAL}")
+
+if len(sys.argv) < 2:
+    exit(0)
+sys.argv.pop(0)
+sys.argv.reverse()
+result = ' '.join(sys.argv[::-1]).swapcase()
+print(f"{result[::-1]}")
