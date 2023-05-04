967e93b89cddb297626dea64b8a39434c0a4c271
maxime-42 <mkayumba@student.42.fr>
2023-05-04 08:38:40 +0200

    Add files via upload

---
+++ day01/ex02/test.py
@@ -0,0 +1,16 @@
+from vector import Vector
+
+# print("Range:")
+# range_vector = Vector((5, 10))
+# print("column:")
+# column = Vector([[1.], [2.], [3.], [0]])
+
+print("# Column vector of shape n * 1")
+v1 = Vector([[0.0], [1.0], [2.0], [3.0]])
+# v2 = v1 * 5
+# print(v2)
+
+# print("# Row vector of shape 1 * n")
+v1 = Vector([[0.0, 1.0, 2.0, 3.0]])
+v2 = v1 * 5
+# # print(v2)
